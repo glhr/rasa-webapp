5926414781b2306e8b8aee5e58f85fdaa73f6e32
glhr <grenblot@gmail.com>
2020-05-03 13:39:33 +0200

    working TTS

--- static/js/tts.js
+++ static/js/tts.js
@@ -1,16 +1,84 @@
-var googleTTS = require('google-tts-api');
+import Speech from "speak-tts";
 
+const _addVoicesList = voices => {
+  const list = window.document.createElement("div");
+  let html =
+    '<select id="languages"><option value="">autodetect language</option>';
+  voices.forEach(voice => {
+    html += `<option value="${voice.lang}" data-name="${voice.name}">${
+      voice.name
+    } (${voice.lang})</option>`;
+  });
+  list.innerHTML = html;
+  window.document.body.appendChild(list);
+};
 
+const speech = new Speech();
+
+function _init() {
+  speech
+    .init({
+      volume: 0.5,
+      lang: "en-GB",
+      rate: 1,
+      pitch: 1,
+      //'voice':'Google UK English Male',
+      //'splitSentences': false,
+      listeners: {
+        onvoiceschanged: voices => {
+          console.log("Voices changed", voices);
+        }
+      }
+    })
+    .then(data => {
+      console.log("Speech is ready", data);
+      _addVoicesList(data.voices);
+    })
+    .catch(e => {
+      console.error("An error occured while initializing : ", e);
+    });
+  const text = speech.hasBrowserSupport()
+    ? "Hurray, your browser supports speech synthesis"
+    : "Your browser does NOT support speech synthesis. Try using Chrome of Safari instead !";
+  console.log(text);
+}
 
 export function sayText(text) {
-  console.log("TTS: " + text)
-  googleTTS(text, 'en', 1)   // speed normal = 1 (default), slow = 0.24
-  .then(function (url) {
-    console.log(url); // https://translate.google.com/translate_tts?...
-    var a = new Audio(url);
-    a.play();
-  })
-  .catch(function (err) {
-    console.error(err.stack);
-  });
+  const languages = document.getElementById("languages");
+  const language = languages.value;
+  const voice = languages.options[languages.selectedIndex].dataset.name;
+  if (language) speech.setLanguage(languages.value);
+  if (voice) speech.setVoice(voice);
+  speech
+    .speak({
+      text: text,
+      queue: false,
+      listeners: {
+        onstart: () => {
+          console.log("Start utterance");
+        },
+        onend: () => {
+          console.log("End utterance");
+        },
+        onresume: () => {
+          console.log("Resume utterance");
+        },
+        onboundary: event => {
+          console.log(
+            event.name +
+              " boundary reached after " +
+              event.elapsedTime +
+              " milliseconds."
+          );
+        }
+      }
+    })
+    .then(data => {
+      console.log("Success !", data);
+    })
+    .catch(e => {
+      console.error("An error occurred :", e);
+    });
 }
+
+_init();
